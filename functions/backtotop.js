452634420056bfed92d4091bd080e14f872ab478
Malcolm Bishop <malcolm@thebishops2010.co.uk>
2025-04-02 20:25:03 +0100

    update button

--- functions/backtotop.js
+++ functions/backtotop.js
@@ -1,24 +1,15 @@
-// Get the button element
-let backToTopBtn = document.getElementById("backToTopBtn");
-
-// Show the button when scrolling down
 window.onscroll = function() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    backToTopBtn.style.display = "block";
-  } else {
-    backToTopBtn.style.display = "none";
-  }
-};
-function scrollFunction() {
   const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   if (scrollTop > 20) {
-      mybutton.style.display = "block";
+      document.getElementById("backToTopBtn").style.display = "block";
   } else {
-      mybutton.style.display = "none";
+      document.getElementById("backToTopBtn").style.display = "none";
   }
-}
-// Scroll to the top when clicking the button
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+};
+
+function scrollToTop() {
+  window.scrollTo({
+      top: 0,
+      behavior: "smooth" // Smooth scrolling effect
+  });
 }
